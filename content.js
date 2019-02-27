36af7ce11de73b42357b8cf873c01faa8de6bad9
Tom Söderlund <tom@tomsoderlund.com>
2019-02-27 13:13:13 +0100

    Improved output and defaults

--- content.js
+++ content.js
@@ -3,12 +3,12 @@ console.log('Simple Scraper running')
 // content.js
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.message === 'clicked_browser_action') {
-    const defaultSelector = '.features-more-wrapper h2 a' // '.myclass div'
+    const defaultSelector = '.myclass div'
     const selector = window.prompt('What’s the CSS selector?', defaultSelector)
     const items = document.querySelectorAll(selector)
     const itemsArray = []
     items.forEach((item, index) => {
-      console.log(index, item)
+      console.log(`#${index + 1}:`, item)
       itemsArray.push(item.toString())
     })
     window.alert(itemsArray.join('\n'))
